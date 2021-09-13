5d09e0639f3cca0efea423eac403b2f8d4b1e009
Fahimict <fa9010583@gmail.com>
2021-09-13 19:17:26 +0600

    Add files via upload

---
+++ Write a c Program on 2 1+(1+2)+(1+2+3) ……………..(1+2+…..+n).c
@@ -0,0 +1,17 @@
+///Write a c Program on 2 1+(1+2)+(1+2+3) ……………..(1+2+…..+n)
+#include<stdio.h>
+void main()
+{
+    int sum=0,i,j,n;
+    scanf("%d",&n);
+
+    for(i=1;i<=n;i++)
+    {
+        for(j=1;j<=i;j++)
+        {
+            sum=sum+j;
+        }
+    }
+
+    printf("Sum= %d ",sum);
+}

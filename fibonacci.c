03c2a7add84169a323b79d9d177f56ef52be2160
Fahimict <fa9010583@gmail.com>
2021-10-03 12:26:24 +0600

    Add files via upload
    Find nth fibonacci number

---
+++ fibonacci.c
@@ -0,0 +1,20 @@
+/// fibonacci check
+#include<stdio.h>
+void main()
+{
+    int f=0,s=1,n,t,i;
+
+    n=4;
+
+    for(i=0;i<n;i++)
+    {
+        if(i<=1)
+            t=i;
+        else{
+            t=f+s;
+            f=s;
+            s=t;
+        }
+    }
+    printf("%d ",t);
+}

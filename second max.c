a8f4e438ef53099a4d5892f704c59dec57d177d1
Fahimict <fa9010583@gmail.com>
2021-12-19 20:19:29 +0600

    Update second max.c

--- second max.c
+++ second max.c
@@ -4,20 +4,25 @@ int main()
     int n,i,max1,max2;
     scanf("%d",&n);
     int a[n];
-
     for(i=0;i<n;i++)
         scanf("%d",&a[i]);
     max1=a[0];
-    max2=a[0];
-    for(i=1;i<n;i++)
+
+
+    for(i=0;i<n;i++)
     {
         if(a[i]>max1)
             max1=a[i];
     }
-    for(i=1;i<n;i++)
+    for(i=0;i<n;i++)
+    {
+        if(a[i]<max1)
+            max2=a[i];
+    }
+    for(i=0;i<n;i++)
     {
         if(a[i]>max2&&a[i]<max1)
             max2=a[i];
     }
-    printf("Max1 = %d max2 = %d",max1,max2);
+    printf("%d",max2);
 }

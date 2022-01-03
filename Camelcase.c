d633809c264fc5e4b2e4b006ce6924b75d1b4c7f
Fahimict <fa9010583@gmail.com>
2022-01-03 13:17:44 +0600

    Add files via upload

---
+++ Camelcase.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<string.h>
+void main()
+{
+    char s[50];
+    gets(s);
+    int i=0,j=0,n=strlen(s);
+    for(i=0;i<n;i++)
+    {
+        if(s[i]==' ')
+        s[i+1]=toupper(s[i+1]);
+        else
+            s[j++]=s[i];
+    }
+    s[j]='\0';
+    printf("%s",s);
+}

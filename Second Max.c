0a6c6ac1545078773c2237709bd7dd70b9bf6b98
Fahimict <fa9010583@gmail.com>
2022-01-22 19:51:35 +0600

    Update Second Max.c

--- Second Max.c
+++ Second Max.c
@@ -1,27 +1,26 @@
 #include<stdio.h>
-int main()
+void main()
 {
     int n,i,max1,max2;
     scanf("%d",&n);
     int a[n];
-    for(i=0;i<n;i++)
-        scanf("%d",&a[i]);
+    for(i=0;i<n;i++)scanf("%d",&a[i]);
     max1=a[0];
-
-
     for(i=0;i<n;i++)
     {
-        if(a[i]>max1){
-           max2=max1;
-           max1=a[i];
-        }
-
+        if(a[i]>max1)
+            max1=a[i];
     }
-
     for(i=0;i<n;i++)
     {
-        if(a[i]>max2&&a[i]<max1)
+        if(a[i]<max1)
             max2=a[i];
     }
-    printf("Second max = %d",max2);
+    for(i=0;i<n;i++)
+    {
+        if(a[i]<max1&&a[i]>max2)
+            max2=a[i];
+    }
+
+    printf("Second Max = %d\n",max2);
 }
